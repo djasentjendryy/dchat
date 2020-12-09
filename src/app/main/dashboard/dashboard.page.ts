147e7920bc0d2f225639f73346b16e9ed17a055b
stevemanumpil <manumpil_steve@yahoo.co.id>
2020-12-09 23:44:43 +0700

    nampilin recent chat di dashboard

--- src/app/main/dashboard/dashboard.page.ts
+++ src/app/main/dashboard/dashboard.page.ts
@@ -7,6 +7,7 @@ import {RS} from '../../model/rumah_sakit.model';
 import {RumahSakitService} from '../../service/rumah-sakit.service';
 import {map} from 'rxjs/operators';
 import { AngularFireDatabase } from '@angular/fire/database';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.page.html',
@@ -21,8 +22,9 @@ export class DashboardPage implements OnInit {
     searching: any = false;
     public searchControl: FormControl;
     public searchTerm = '';
-    currUser: string = JSON.parse(localStorage.getItem('currUser')).nama;
-    recentChat: any;
+    currUserId: string;
+    recentChat: any = '';
+    chatSubs: Subscription;
     constructor(
         private rsService: RumahSakitService,
         private userNusaService: UserNusaService,
@@ -34,91 +36,44 @@ export class DashboardPage implements OnInit {
         this.searchControl = new FormControl();
     }
 
-  ngOnInit() {
-    this.roomService.getAllRoom().snapshotChanges().pipe(
-        map(changes => changes.map( c => ({key: c.payload.key, ...c.payload.val()}))),
-      )
-      .subscribe(data => {
-        const tempdata = data.filter( chat => {
-          return chat.messages
-        })
-  
-        tempdata.forEach( val => {
-          val.participant = val.participant.filter( user => { return user != this.currUser})
-          this.db.list('/room/'+val.key+'/messages').query.orderByKey().limitToLast(1).on('child_added', ref => {
-            val.messages = ref.val()
-          })
-        })
-  
-        this.recentChat = tempdata
-      })
+    ngOnInit() {
+        this.rs = this.rsService.getAllRS();
+    }
 
-
-
-    this.rs = this.rsService.getAllRS();
-    this.user = JSON.parse(localStorage.getItem('currUser'));
-    if(this.lang){
-          this.userNusaService.getAllUser().snapshotChanges().pipe(
-              map( changes =>
-                  changes.map(c => ({
-                      imgUrl: '../../../assets/img/logo.png',...c.payload.val()
-                  }))
-              )
-          ).subscribe( ({filter}) => {
-              this.Dummy = filter(user => {
-                  return user.lang.includes(this.lang) && user.nama != this.currUser
-              });
-          });
-      }
-      else{
-          this.userNusaService.getAllUser().snapshotChanges().pipe(
-              map( changes =>
-                  changes.map(c => ({
-                      imgUrl: '../../../assets/img/logo.png', ...c.payload.val()
-                  }))
-              )
-          ).subscribe( ({filter}) => {
-              this.Dummy = filter(user => {
-                  // tslint:disable-next-line:triple-equals
-                  return user.nama != this.currUser;
-              });
-          });
-      }
-  }
     setFilteredItems(searchTerm) {
         this.Dummy = this.userNusaService.filterItems(searchTerm);
     }
 
-    chat(user: string){
-        let currkey = '';
-        this.roomService.getRoom()
-            .then( snapshot => {
-                const data = snapshot.val();
-                if (data){
-                    for ( const [key, value] of Object.entries(data)){
-                        // tslint:disable-next-line:max-line-length
-                        // if(value['participant'][0] == this.currUser && value['participant'][1] == user || value['participant'][0] == user && value['participant'][1] == this.currUser){
-                            currkey = key;
-                            break;
-                        // }
-                    }
-                }
+    ionViewWillEnter(){
+        this.currUserId = localStorage.getItem('UID')
 
-                if(currkey){
-                    this.navCtrl.navigateForward('/main/roomchat/'+currkey)
-                    this.onClose();
-                }
-                else{
-                    const key = this.roomService.setRoom(this.currUser, user)
-                    this.navCtrl.navigateForward('/main/roomchat/'+key)
-                    this.onClose();
-                }
-            });
-  }
+        this.chatSubs = this.roomService.getAllRoom().snapshotChanges().pipe(
+            map(changes => changes.map( c => ({key: c.payload.key, ...c.payload.val()}))),
+          )
+          .subscribe(data => {
+            const tempdata = data.filter( chat => {
+              return chat.messages && chat.participant.includes(this.currUserId)
+            })
+      
+            tempdata.forEach( val => {
+              const user_key = val.participant.filter(key => key != this.currUserId).pop();
+              this.db.list('/userDchat/' + user_key).query.once('value', ref => {
+                val.participant = ref.val();
+              });
+              this.db.list('/room/' + val.key + '/messages').query.orderByKey().limitToLast(1).on('child_added', ref => {
+                val.messages = ref.val();
+              });
+            })
 
-  ionViewWillEnter(){
-    this.user = JSON.parse(localStorage.getItem('currUser'));
-  }
+            this.recentChat = tempdata
+          })
+    }
+
+    ionViewWillLeave(){
+        if(this.chatSubs){
+            this.chatSubs.unsubscribe();
+        }
+    }
 
     onClose(){
         this.modalCtrl.dismiss();
